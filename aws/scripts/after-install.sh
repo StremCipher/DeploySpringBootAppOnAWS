86cf39af1e1ae252397a52119261df5f0b931bbc
StremCipher <ammitkumar0123@gmail.com>
2022-08-25 09:58:10 +0530

    s3 added back

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-# aws s3 cp s3://codedeploystack-webappdeploymentbucket-1thuus91akces/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-1thuus91akces/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
-# chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+chown -R tomcat:tomcat /usr/local/tomcat9/webapps
